cd278f3a6c27edd3e0883212c5bf50efe79e64a9
Sang Vo Hoang <sangvohoang@vnoss.org>
2018-06-02 22:23:16 +0700

    first commit

---
+++ sshd-gen.sh
@@ -0,0 +1,13 @@
+echo ${create_user} ${user} ${pwuser}
+[ ${create_user} ] && \
+		apk --update add --no-cache sudo && \
+		echo "%sshuser ALL=(ALL) NOPASSWD:ALL"  | tee -a /etc/sudoers	&& \
+		addgroup -g 1000 -S sshuser && \
+		adduser -u 1000 -S ${user} -G sshuser -s /bin/bash  && \
+		echo "${user}:${pwuser}" | chpasswd && \
+		su ${user} -c "mkdir -p /home/${user}/.ssh/"
+        # mkdir -p /home/${user}/.ssh/
+
+export create_user=false
+
+/usr/sbin/sshd -D
